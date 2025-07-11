e0331c7b124af51c9846f4c784f3316b769705a6
AndrewACM <andruchacon14@gmail.com>
2025-07-10 20:09:04 -0400

    feature: andru chacon agrego un archivo

--- andruchacon.py
+++ andruchacon.py
@@ -1,5 +1,11 @@
 
-print("GitFlow")
-print("Andru Chacon")
-print("Seccion C")
-print("Introduccion al Desarrollo de Software")
+print("Funcion de resta de dos numeros")
+
+def resta_num (n1, n2):
+    return n1 - n2
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+resta = resta_num(num1, num2)
+
+print("La resta entre ", num1," y ", num2," es: ",resta)
